961206f648c7e81d1c61d9320d0c6aee83393a8e
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-04-27 23:41:32 -0400

    addEmployee and addRole functions

--- app.js
+++ app.js
@@ -1,26 +1,63 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
 
-var connection = mysql.createConnection({
-  host: "localhost",
+const connection = mysql.createConnection({
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "",
-  database: "workforceDB"
+    // Your password
+    password: "",
+    database: "workforceDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("Connected to DB!");
-    addDepartment();
+    console.log("Connected to workforceDB!");
+    // addDepartment();
+    // addRole();
+    addEmployee();
     // connection.end();
 });
+
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What is the title of the role you'd like to add?"
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary of this new role?"
+            },
+            {
+                name: "department",
+                type: "input",
+                message: "What's the number of the department "
+            }
+        ])
+        .then(function (answer) {
+            let query = "INSERT INTO role SET ?"
+            connection.query(query,
+                {
+                    title: answer.title,
+                    salary: answer.salary,
+                    department_id: answer.department
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("The new role was added!");
+                });
+        });
+};
 
 function addDepartment() {
     inquirer
@@ -31,15 +68,64 @@ function addDepartment() {
                 message: "What is the name of the department you'd like to add?"
             }
         ])
-        .then(function(answer) {
+        .then(function (answer) {
             let query = "INSERT INTO department SET ?"
-            connection.query(query, 
+            connection.query(query,
                 {
                     name: answer.department
                 },
-                function(err) {
+                function (err) {
                     if (err) throw err;
                     console.log("The new department was added!");
+                });
         });
-    });
+};
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                name: "first",
+                type: "input",
+                message: "What is the first name of the new employee?"
+            },
+            {
+                name: "last",
+                type: "input",
+                message: "What is the last name of the new employee?"
+            },
+            {
+                name: "role",
+                type: "input",
+                message: "What role does the new employee have?"
+            },
+            {
+                name: "confirmManager",
+                type: "list",
+                message: "Does the employee have a manager?",
+                choices: ["Yes", "No"]
+            },
+            {
+                name: "manager",
+                type: "input",
+                message: "What's the name of the manager?",
+                when: function(answer) {
+                    return answer.confirmManager === "Yes";
+                }
+            }
+        ])
+        .then(function (answer) {
+            let query = "INSERT INTO employee SET ?"
+            connection.query(query,
+                {
+                    first_name: answer.first,
+                    last_name: answer.last,
+                    role_id: answer.role,
+                    manager_id: answer.manager
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("The new role was added!");
+                });
+        });
 };
